8001ca87b95639b19d33e847877a53f9a4c064be
koyo-miyagi <gate8218@gmail.com>
2022-05-19 00:15:44 +0900

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Merry Christmas!'));
+app.get('/', (req, res) => res.send('Thank you very match!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
